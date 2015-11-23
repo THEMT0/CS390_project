766e7e2753252d6b87f8096d1137052efc6aed94
agschilthelm <aschilt2@gmu.edu>
2015-11-23 12:40:16 -0500

    TEST profile pic button pressing

--- app/src/main/java/com/example/emundo/cs390_project/CreateProfile.java
+++ app/src/main/java/com/example/emundo/cs390_project/CreateProfile.java
@@ -9,6 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.app.Activity;
+import java.io.InputStream;
+import android.app.ProgressDialog;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+//import me.vable.android.CS390_project.data.User;
+//import me.vable.android.viewandlayoutlessons.data.service.UserService;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -17,12 +27,16 @@ public class CreateProfile extends AppCompatActivity {
     EditText passwordTextBox;
     EditText bioTextBox;
     ImageView profileImage;
+    private static final int SELECT_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
 
+        profileImage = (ImageView) findViewById(R.id.imageview_profile);
+
+        //Create handler for clicking on submit button, linked to UserList activity
         submitButton = (Button) findViewById(R.id.create_profile_button);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +45,23 @@ public class CreateProfile extends AppCompatActivity {
             }
         });
 
+        profileImage.setOnClickListener(onClickProfileImageViewListener);
+
+    }
+
+    //listener for when profile pic is clicked on
+    View.OnClickListener onClickProfileImageViewListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            chooseImage();
+        }
+    };
+
+    //method to choose the image from the users device
+    private void chooseImage()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_PICTURE);
     }
 
     @Override
@@ -53,5 +84,35 @@ public class CreateProfile extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //takes the image from file and puts in in the profile pic box
+    private Bitmap bitmap;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK)
+            try {
+                // We need to recyle unused bitmaps
+                if (bitmap != null) {
+                    bitmap.recycle();
+                }
+                InputStream stream = getContentResolver().openInputStream(data.getData());
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize=2;
+                bitmap = BitmapFactory.decodeStream(stream, null, options);
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                double scale = 100.0/height;
+                height = (int)(height*scale);
+                width = (int)(width*scale);
+                bitmap = Bitmap.createScaledBitmap(bitmap, width,height, false);
+                stream.close();
+                profileImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
